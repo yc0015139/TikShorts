221d4c884efe5d94f35a6900b89d62a21c84da55
yc.lu <a12125577@gmail.com>
2023-10-17 13:59:25 +0800

    TS-2: Setup material theme for xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,8 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation ("com.google.android.material:material:1.10.0")
 
     val navVersion = "2.7.4"
     implementation("androidx.navigation:navigation-compose:$navVersion")
