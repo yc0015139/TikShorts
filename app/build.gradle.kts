ed90e3e4e857796c03869460be07878e4cd1990c
yc.lu <a12125577@gmail.com>
2023-10-26 13:44:02 +0800

    TS-4: Update libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,15 +55,19 @@ kapt {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation ("com.google.android.material:material:1.10.0")
+
+    // compose
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.compose.ui:ui-viewbinding:1.5.4")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3:1.1.2")
-    implementation ("com.google.android.material:material:1.10.0")
 
     // Fragment delegate
     implementation("com.github.Zhuinden:fragmentviewbindingdelegate-kt:1.0.2")
@@ -76,6 +80,7 @@ dependencies {
     val hiltVersion = "2.44"
     implementation("com.google.dagger:hilt-android:$hiltVersion")
     kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
     // Android media3
     val media3Version = "1.1.1"
@@ -87,7 +92,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    implementation("androidx.compose.ui:ui-viewbinding:1.5.3")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
