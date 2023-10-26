c12fc27266b96887880c0ed716bebb4f6d1c0bc7
yc.lu <a12125577@gmail.com>
2023-10-26 17:51:53 +0800

    TS-4: The ComposeSimulatedLifecycle is buggly

--- app/src/main/java/yc/dev/tikshorts/utils/compose/ComposeSimulatedLifecycle.kt
+++ app/src/main/java/yc/dev/tikshorts/utils/compose/ComposeSimulatedLifecycle.kt
@@ -3,52 +3,49 @@ package yc.dev.tikshorts.utils.compose
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LifecycleRegistry
 
+
+/**
+ * Emulate the lifecycle for lifecycle-aware components
+ *
+ * BUGGLY. DO NOT USE IT.
+ *
+ * TODO: Fix it
+ *
+ * */
 @Composable
-fun composeSimulatedLifecycle(): Lifecycle = LifecycleRegistry(object : LifecycleOwner {
-    private val registry = LifecycleRegistry(this)
-    override val lifecycle: Lifecycle
-        get() = object : Lifecycle() {
+fun composeSimulatedLifecycle(): Lifecycle {
+    val lifecycleOwner = rememberUpdatedState(object : LifecycleOwner {
+        private val registry = LifecycleRegistry(this)
+        override val lifecycle: Lifecycle
+            get() = registry
+    })
 
-            override val currentState: State
-                get() = registry.currentState
-
-            override fun addObserver(observer: LifecycleObserver) {
-                registry.addObserver(observer)
-            }
-
-            override fun removeObserver(observer: LifecycleObserver) {
-                registry.removeObserver(observer)
-            }
-        }
-}).also {
-    LaunchedEffect(Unit) {
-        it.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
-        it.handleLifecycleEvent(Lifecycle.Event.ON_START)
-    }
+    val lifecycleRegistry = lifecycleOwner.value.lifecycle as LifecycleRegistry
 
     val activityLifecycleOwner = LocalLifecycleOwner.current
     DisposableEffect(Unit) {
         val observer = object : DefaultLifecycleObserver {
             override fun onResume(owner: LifecycleOwner) {
-                it.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+                lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
             }
 
             override fun onPause(owner: LifecycleOwner) {
-                it.handleLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+                lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_PAUSE)
             }
         }
         activityLifecycleOwner.lifecycle.addObserver(observer)
-
         onDispose {
-            it.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+            lifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
             activityLifecycleOwner.lifecycle.removeObserver(observer)
         }
     }
+
+    return lifecycleRegistry
 }
